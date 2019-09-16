9f5ba73cd7664b08b16d9396aaf65253ee24b41b
ellievanb <54558569+ellievanb@users.noreply.github.com>
2019-09-16 08:02:03 -0400

    completes LICENSE file, updates template

--- template.py
+++ template.py
@@ -9,21 +9,22 @@ logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=loggin
 logger = logging.getLogger(__name__)
 
 
-
-
-def main():
-    game = {}
-    with open('zork.json') as json_file:
-        game = json.load(json_file)
-    # Your game goes here!
-
-    current = 'WHOUS'
-
-
-    return True
-
-
-
 #if we are running this from the command line, run main
 if __name__ == '__main__':
-	main()
+
+    birthdays = {
+        'Maddie Stacey': '09/13/1998',
+        'Madison Young': '05/05/1999',
+        'Elizabeth VanBuskirk': '06/02/1999',
+        }
+
+    print('Welcome to the birthday dictionary. We know the birthdays of:')
+    for name in birthdays:
+        print(name)
+
+    print('Who\'s birthday do you want to look up?')
+    name = input()
+    if name in birthdays:
+        print ('{}\'s birthday is {}.'.format(name,birthdays[name]))
+    else:
+        print('Sadly, we don\'t have {}\'s birthday.'.format(name))  
